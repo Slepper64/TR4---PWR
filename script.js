4801553b3edc2a584ac00947673dabd00abf06e8
Slepper64 <169095658+Slepper64@users.noreply.github.com>
2024-05-26 11:22:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openLink() {
+    window.open('https://matias.me/nsfw/', '_blank');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("namorada").addEventListener("click", function() {
+        alert("Pra que você quer saber? EEMMM??!");
+    });
+});
